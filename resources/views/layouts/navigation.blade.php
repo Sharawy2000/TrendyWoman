0ed7c0257f5cca8df4f2303bfa12ec60249f458d
Wesam El-ShaȜrawi <mr.wesamahmed@gmail.com>
2024-07-16 12:53:36 +0300

    new

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,11 @@
                     </x-nav-link>
                 </div>
                 @endif
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
